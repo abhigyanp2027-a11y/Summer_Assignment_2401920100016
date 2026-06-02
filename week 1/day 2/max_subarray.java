5eb708bef35b3271ab6ceeff667b03066dd3fcd4
abhigyanp2027-a11y <abhigyanp.2027@gmail.com>
2026-06-02 20:15:31 +0530

    Implement max subarray

---
+++ week 1/day 2/max_subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+         int r=nums[0];
+        int max=nums[0];
+
+        for(int i =1;i<nums.length;i++) {
+            max=Math.max(max+nums[i],nums[i]);
+            r=Math.max(r,max);
+        }
+        return r;
+        
+    }
+}
