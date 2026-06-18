01cbcd1f4a68573b7476d75f6eef973b8df37f4f
abhigyanp2027-a11y <abhigyanp.2027@gmail.com>
2026-06-18 09:59:54 +0530

    .

---
+++ week 3/day 3/next_greater_element_I.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = -1;
+            boolean found = false;
+
+            for (int j = 0; j < nums2.length; j++) {
+                if (found && nums2[j] > nums1[i]) {
+                    ans[i] = nums2[j];
+                    break;
+                }
+                if (nums2[j] == nums1[i]) {
+                    found = true;
+                }
+            }
+        }
+        return ans;
+    }
+}
